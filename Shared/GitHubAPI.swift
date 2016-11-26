5dbf7486476e41c21962f4949b6ce87354b68791
Marin Todorov <marin@underplot.com>
2016-11-26 11:23:43 +0100

    renames a method to better describe what it does

--- Shared/GitHubAPI.swift
+++ Shared/GitHubAPI.swift
@@ -28,7 +28,7 @@ protocol RepoProvider {
 class GitHubAPI: RepoProvider {
     
     // gets a list of repos from github and converts them to Repository objects
-    func getRepos(count: Int = 100, _ completion: @escaping ([Repository])-> Void) {
+    func updateRepos(count: Int = 100, _ completion: @escaping ([Repository])-> Void) {
         
         let reposUrlString = String(format: "https://api.github.com/search/repositories?q=language:swift&per_page=%d", count)
         let request = URLRequest(url: URL(string: reposUrlString)!)
